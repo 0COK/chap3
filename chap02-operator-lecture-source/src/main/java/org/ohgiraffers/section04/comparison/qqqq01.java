da9016d4bf1000b95bba9fcbfd4395c24f1ef549
0COK0 <wlsgus1208@gmail.com>
2024-01-30 17:56:05 +0900

    commit

---
+++ chap02-operator-lecture-source/src/main/java/org/ohgiraffers/section04/comparison/qqqq01.java
@@ -0,0 +1,63 @@
+package org.ohgiraffers.section04.comparison;
+
+public class qqqq01 {
+    public static void main(String[] args) {
+        // 숫자 100을 자신이 아는 5가지 방법으로 출력하시오.
+
+        int num1 = 100;
+        int num2 = 200;
+        int num3 = 2;
+        int num4 = 10;
+
+
+        System.out.println("숫자 : " + num1);
+        System.out.println("숫자 : " + (num2 - num1));
+        System.out.println("숫자 : " + (num4 * num4));
+        System.out.println("숫자 : " + (num2 / num3));
+        System.out.println("숫자 : " + (num1 % num2));
+        /*
+        1. 문자형 자료형을 선언한후 'a' 값을 대입하여 초기화한다.
+        2. 생성된 문자형 변수를 실수 자료형으로 형변환 한다.
+        3. 보기의 정수형 변수 num값에서 단항 증감연산자를 이용해 1을 뺀값과
+        실수로 형변환 된 문자형 변수를 논리 비교연산자를 이용해 비교하여
+        true 값이 출력되도록 실수형 변수값을 증감/형변환하여 출력문을 만드세요.
+    */
+
+    /* ===========출력예시============
+           num = 98
+           형변환 된 변수명 = 98.0
+           true
+    */
+        // 보기
+        // int num = 99;
+
+        // 문자형 자료형을 선언
+        char ch = 'a';  // 97
+        double  = ch;
+
+
+        int num = 99;
+
+
+//        ++1;
+//        System.out.println("num : " + num);
+
+        /* 증감 연산자를 단항으로 사용하여 정수 a와 b를 출력 한 뒤
+         * 기존 a = 23; / b = 24;
+         * 증감 연산자를 240129일자에 배운 다른 연산자와 사용하여 result 와 firstNum/secondNum을 출력하시오
+         * 참고 int num = 30;
+         */
+
+        /* ======= 출력 내용 =======
+            a = 24
+            b = 23
+            result1 = 90
+            firstNum = 31
+            result2 = 93
+            secondNum = 31
+         */
+
+
+
+    }
+}
